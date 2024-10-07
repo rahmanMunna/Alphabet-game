f0d7752f6d652a67b0daa65e3d92453dc2bf5fc2
Munna <habiburmunna0@gmail.com>
2024-10-08 00:27:57 +0600

    first commit

---
+++ Styles/style.cs
@@ -0,0 +1,9 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+
+.font-anton {
+  font-family: "Anton", sans-serif;
+  font-weight: 800;
+  font-style: normal;
+}
